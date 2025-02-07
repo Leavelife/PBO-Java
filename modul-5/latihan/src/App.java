71cceba15c8b53d348df07ee2daec1cdf9cb434b
Leavelife <nurusyah369@gmail.com>
2025-02-07 20:08:46 +0700

    add latihan 5. membuat text field

--- modul-5/latihan/src/App.java
+++ modul-5/latihan/src/App.java
@@ -1,56 +1,21 @@
-//implementasi sederhana penggunaan konsep OOP
-
-//ini CLASS
-class Users {
-    //ini encapsulsi
-    protected String username;
-    protected String email;
-    protected String password;
-    //ini method
-    void register(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-    void notifikasi() {
-        System.out.println("\nUsername: " + username);
-        System.out.println("Email: " + email);    
-    }
-}
-//ini SubClass Inheritence dari Class Users
-class Pembeli extends Users{
-    @Override
-    void notifikasi() {
-        super.notifikasi();
-        System.out.println("register dilakukan oleh pembeli\n");
-    }
-}
-class Penjual extends Users{
-    @Override
-    void notifikasi() {
-        super.notifikasi();
-        System.out.println("register dilakukan oleh penjual\n");
-    }
-    
-}
-class Adminstrator extends Users{
-    @Override
-    void notifikasi() {
-        super.notifikasi();
-        System.out.println("register dilakukan oleh administrator\n");
-    }
-    
-}
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class App {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JTextField textfield = new JTextField();
+        //inisialisasi nama jendela
+        frame.setTitle("membuat text field");
+        //mengatur posisi dan ukuran jendela
+        frame.setBounds(450, 250, 500, 300);
+        //menambahkan text field kedalam jendela
+        frame.add(textfield);
+        //mengatur ukuran text field
+        textfield.setBounds(50, 50, 150, 25);
 
-        String username = "saya";
-        String email = "saya@gmail.com";
-        String password = "saya123";
-
-        Users user = new Adminstrator();
-        user.register(username, email, password);
-        user.notifikasi();
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
---
+++ modul-5/latihan/src/readme.md
@@ -0,0 +1,21 @@
+
+### 1. Membuat JFrame (Window)
+
+```json 
+JFrame frame = new JFrame();
+```
+`JFrame:` Kelas dari library Swing yang digunakan untuk membuat jendela (window).
+
+`frame:` Objek dari kelas JFrame yang akan menjadi jendela utama aplikasi.
+***
+### 3. Membuat JTextField (Text Field)
+
+```json
+JTextField textfield = new JTextField();
+```
+`JTextField:` Kelas dari library Swing yang digunakan untuk membuat komponen input teks (text field).
+
+
+`textfield:` Objek dari kelas JTextField yang akan digunakan untuk memasukkan teks.
+
+
