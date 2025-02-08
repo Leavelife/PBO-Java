a221ca2f9392dd761658b25e0e3c96d79dcda7a7
Leavelife <nurusyah369@gmail.com>
2025-02-08 20:34:36 +0700

    add tugas praktikum 3

--- modul-2/tugas praktikum/src/App.java
+++ modul-2/tugas praktikum/src/App.java
@@ -1,23 +1,28 @@
+import java.io.*;
 
 public class App {
-    public static int kali(int a, int b) {
-        return a * b;
-    }
-    public static double kali(double a, double b) {
-        return a * b;
-    }
-    public static int kali(int a, int b, int c) {
-        return a * b * c;
+    public static String read() {
+        String baca = "";
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buf = new BufferedReader(input);
+        try {
+            baca = buf.readLine();
+        } catch (IOException e) {
+            System.out.println("error!");
+        }
+        return baca;
     }
     public static void main(String[] args) throws Exception {
-        int a = 9, b = 1, c = 2;
-        double d = 9.0, e = 2.0;
+        String nama, karakter, hobi;
+        System.out.println("\nMasukkan nama mu: ");
+        nama = read();
+        System.out.println("Masukkan karaktermu mu: ");
+        karakter = read();
+        System.out.println("Masukkan hobi mu: ");
+        hobi = read();
 
-        System.out.println("1. " + kali(a, b));
-        System.out.println("2. a(" + a + ") + b(" + b + ") = " + kali(a, b));
-        System.out.println("3. " + kali(a, b, c));
-        System.out.println("4. a(" + a + ") + b(" + b + ") + c(" + c + ") = " + kali(a, b, c));
-        System.out.println("5. " + kali(d, e));
-        System.out.println("6. a(" + d + ") + b(" + e + ") = " + kali(d, e));
+        System.out.println("\nHalo... nama ku " + nama);
+        System.out.println("Aku itu orang yang sangat " + karakter);
+        System.out.println("Dan hobi ku adalah " + hobi);
     }
 }
