cb6ff1ee78da9605ac884d6aef841137738b047d
Leavelife <nurusyah369@gmail.com>
2025-02-09 21:08:58 +0700

    add latihan 1

--- modul-3 inheritence/latihan/src/App.java
+++ modul-3 inheritence/latihan/src/App.java
@@ -1,34 +1,53 @@
-import java.io.*;
+class Person {
+    protected String name;
+    protected int age;
 
+    //setter method
+    public void setInput(String noKaryawa, String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    //getter method
+    public String getName() {
+        return name;
+    }
+    //getter method
+    public int getAge() {
+        return age;
+    }
+    public void info() {
+        System.out.println("Nama: " + getName());
+        System.out.println("Usia: " + getAge());
+    }
+}
+//ini class inheritence
+class Employ extends Person {
+    private String noKaryawan;
+
+    @Override 
+    //agar method bisa digunakan oleh objek bertipe data class utama
+    public void setInput(String noKaryawan, String name, int age) {
+        super.setInput(noKaryawan, name, age);
+        this.noKaryawan = noKaryawan;
+    }
+    public String getNoKaryawan() {
+        return noKaryawan;
+    }
+    public void info() {
+        System.out.println("No Karyawan: " + getNoKaryawan());
+        super.info();
+    }
+}
 
 public class App {
-    public static String readInput() {
-        String baca = "";
-            InputStreamReader input = new InputStreamReader(System.in);
-            BufferedReader buf = new BufferedReader(input);
-            try {
-                baca = buf.readLine();
-            } catch (Exception e) {
-                System.out.println("error");
-            }
-        return baca;
-    }
     public static void main(String[] args) throws Exception {
-        String name = "";
-        int nilaiMatematika, nilaiFisika, nilaiKimia;
+        Employ karyawan = new Employ();
+        karyawan.setInput("101", "sayaa", 20);
+        karyawan.info();
 
-        System.out.println("Masukkan nama: ");
-        name = readInput();
-        System.out.println("Masukkan nilai matematika: ");
-        nilaiMatematika = Integer.parseInt(readInput());
-        System.out.println("Masukkan nilai fisika: ");
-        nilaiFisika = Integer.parseInt(readInput());
-        System.out.println("Masukkan nilai kimia: ");
-        nilaiKimia = Integer.parseInt(readInput());
-        System.out.println("\n" + name);
-        System.out.println("Nilai Kamu Adalah: ");
-        System.out.println("Matematika: " + nilaiMatematika);
-        System.out.println("Fisika: " + nilaiFisika);
-        System.out.println("Kimia: " + nilaiKimia);
+        System.out.println("\n");
+        Person karyawan2 = karyawan;    //objek menggunakan polimorphism    
+        karyawan2.setInput("102", "dia", 30);
+        karyawan2.info();
     }
 }
